badfea2ca6153ce35f64d5535dd86b70aca5c3c3
MUHAMMAD_sharib <shareebshaikh12@gmail.com>
2025-06-30 23:21:35 +0530

    Update index.js

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Home from "./components/Home";
+const API = process.env.REACT_APP_API_BASE_URL;
+
+export const fetchSomeData = async () => {
+  const res = await fetch(`${API}/api/some-data`);
+  return res.json();
+};
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
